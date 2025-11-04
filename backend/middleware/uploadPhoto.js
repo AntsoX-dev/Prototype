ca427ff257d2c722c0d466441dbcb44408256131
Harimananafth <harimananafth@gmail.com>
2025-11-04 15:37:02 +0300

    bugfix: middleware

--- backend/middleware/uploadPhoto.js
+++ backend/middleware/uploadPhoto.js
@@ -1,27 +1,22 @@
-const multer = require("multer");
+import multer from "multer";
 
 // 1. MemoryStorage pour stocker le fichier temporairement en RAM (Buffer)
 const storage = multer.memoryStorage();
 
-// 2. accepter que les images
+// 2. Accepter uniquement les images
 const fileFilter = (req, file, cb) => {
   if (file.mimetype.startsWith("image/")) {
     cb(null, true);
   } else {
-    cb(
-      new Error(
-        "Format de fichier non supporté. Veuillez utiliser une image (PNG, JPG)."
-      ),
-      false
-    );
+    cb(new Error("Format non supporté. Utilisez PNG ou JPG."), false);
   }
 };
 
 // 3. 5MB max et un seul fichier nommé 'photo'
 const uploadPhoto = multer({
-  storage: storage,
+  storage,
   limits: { fileSize: 1024 * 1024 * 5 }, // 5MB max
-  fileFilter: fileFilter,
+  fileFilter,
 }).single("photo");
 
-module.exports = uploadPhoto;
+export default uploadPhoto;
